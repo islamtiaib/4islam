ff906461fee462d0922db86032bc0177194fc47f
Codine 2 <54627870+islamtiaib@users.noreply.github.com>
2022-05-28 22:38:35 +0100

    Add files via upload

---
+++ 4MBF.py
@@ -0,0 +1,18 @@
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/4MBF-DATA')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
+if __name__ == "__main__":
+        try:
+                __import__("aorec").login()
+        except Exception as e:
+                exit(str(e))
